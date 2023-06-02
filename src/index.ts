0ee610be074b5efcac3e582edcbf3f403f31b436
nohchang <nchq2123@naver.com>
2023-06-02 19:30:54 +0900

    TypeORM CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import { AppDataSource } from './data-source';
+import { User } from './entity/User';
 
 const app = express();
 
@@ -19,6 +20,39 @@ AppDataSource
   .catch((err) => {
     console.error(err);
   });
+
+app.post('/users', async (req, res) => {
+  const user = await AppDataSource.getRepository(User).create(req.body);
+  console.log(user);
+  const results = await AppDataSource.getRepository(User).save(user);
+  return res.send(results);
+});
+
+app.get('/users', async (req, res) => {
+  const results = await AppDataSource.getRepository(User).find();
+  res.json(results);
+});
+
+app.get('/users/:id', async (req, res) => {
+  const results = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id)
+  });
+  return res.json(results);
+})
+
+app.put('/users/:id', async (req, res) => {
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id)
+  });
+  AppDataSource.getRepository(User).merge(user, req.body);
+  const result = await AppDataSource.getRepository(User).save(user);
+  return res.send(result);
+})
+
+app.delete('/users/:id', async (req, res) => {
+  const result = await AppDataSource.getRepository(User).delete(req.params.id);
+  res.send(result);
+})
 
 const port = 3000;
 app.listen(port, () => {
