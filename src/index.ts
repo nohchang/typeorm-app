f7aefe767afec77725a851fba67c8afb6c7b28de
nohchang <nchq2123@naver.com>
2023-06-02 18:51:23 +0900

    TypeORM 이용해서 앱과 docker postgres 연결

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
@@ -8,9 +9,18 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('running');
-})
+});
+
+AppDataSource
+  .initialize()
+  .then(() => {
+    console.log('성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server Running at http://localhost:${port}`);
-})
+});
